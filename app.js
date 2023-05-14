68394fbc17f381e4f06608909f8ec65d1e0df057
MiMinchev <133584933+MiMinchev@users.noreply.github.com>
2023-05-14 17:45:19 +0300

    Add files via upload
    Курсова работа на Михаил Михайлов Минчев от  специалност КН1, Факултетен №171кнр

---
+++ app.js
@@ -0,0 +1,15 @@
+function home(){
+    location.replace("index.html");
+}
+
+function info(){
+    location.replace("information.html");
+}
+
+function prog(){
+    location.replace("programs.html");
+}
+
+function equip(){
+    location.replace("equipment.html");
+}
